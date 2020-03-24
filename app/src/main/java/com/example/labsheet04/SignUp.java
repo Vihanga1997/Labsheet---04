5ebeae6fd6e2488909d7d320e5898403990cdd95
Vihanga1997 <vihangavithana@gmail.com>
2020-03-24 13:45:38 +0530

    Labsheet 04

---
+++ app/src/main/java/com/example/labsheet04/SignUp.java
@@ -0,0 +1,33 @@
+package com.example.labsheet04;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+    private static final String CHANNEL_ID = "100CH";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        Button cancalButton = (Button) findViewById(R.id.cancelBtn);
+
+        cancalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelIntent = new Intent(SignUp.this, MainActivity.class);
+                startActivity(cancelIntent);
+            }
+        });
+    }
+}
